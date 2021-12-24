034857289cdec278cca334607b4bad2319938d5f
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-12-24 16:36:28 +0700

    convert to es6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
     return <Component {...pageProps} />
 }
 
