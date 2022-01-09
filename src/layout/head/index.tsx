5d61ec54a632659a3b8a7e39aaf2a801c63892b7
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-01-09 08:03:58 +0700

    adding default props

--- src/layout/head/index.tsx
+++ src/layout/head/index.tsx
@@ -7,4 +7,8 @@ const Head = ({ title, description }: HeadProps) => {
     return <NextSeo title={title} description={description} />
 }
 
+Head.defaultProps = {
+    title: 'Hello World',
+}
+
 export default Head
